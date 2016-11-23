4b8ef16de9221b739711b92a41737b5f7ac31c56
milouBCB <bertrand.emile@hotmail.fr>
2016-11-23 15:41:46 +0100

    eval and test for eval

---
+++ scheme/tests_step3/55_fail_eval.scm
@@ -0,0 +1,10 @@
+;;;; En tete pour script de test ;;;;;;;;;;
+; TEST_RETURN_CODE=FAIL
+; TEST_COMMENT="fail eval"
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(eval 4)
+(eval 4 2)
+(eval )
+(eval 4 4 4)
+(eval 4 1.2)
